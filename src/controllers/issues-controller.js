19d09f2cec1170549c0779a60575e68911276295
Maja Hedegard <mh223pi@student.lnu.se>
2021-02-25 08:53:39 +0100

    add issues-controller

---
+++ src/controllers/issues-controller.js
@@ -0,0 +1,26 @@
+/**
+ * Module for the IssuesController.
+ *
+ * @author Maja Hedegärd
+ * @version 1.0.0
+ */
+
+/**
+ * Encapsulates a controller.
+ */
+export class IssuesController {
+  /**
+   * Displays issues.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async index (req, res, next) {
+    try {
+      res.render('snippets/index')
+    } catch (error) {
+      next(error)
+    }
+  }
+}

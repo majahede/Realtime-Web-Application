28ba9a8c2d15bad8b6c5baec1ecc7718454e1361
Maja Hedegard <mh223pi@student.lnu.se>
2021-02-26 09:18:19 +0100

    add schema

---
+++ src/models/issue.js
@@ -0,0 +1,23 @@
+/**
+ * Mongoose model Issue.
+ *
+ * @author Maja Hedegärd
+ * @version 1.0.0
+ */
+
+import mongoose from 'mongoose'
+
+// Create a schema.
+const schema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+}, {
+  timestamps: true
+})
+
+// Create a model using the schema.
+export const Issue = mongoose.model('Issue', schema)

6218811d9a8e848b6b6ab98aaf7734456c3ecd45
Maja Hedegard <mh223pi@student.lnu.se>
2021-03-02 10:54:44 +0100

    set up hook router

---
+++ src/routes/hook-router.js
@@ -0,0 +1,17 @@
+/**
+ * Hook routes.
+ *
+ * @author Maja Hedegärd
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import { HookController } from '../controllers/hook-controller.js'
+import { IssuesController } from '../controllers/issues-controller.js'
+
+export const router = express.Router()
+
+const controller = new HookController()
+const issuesController = new IssuesController()
+
+router.post('/issue', controller.authorize, controller.index, issuesController.create)
--- src/routes/router.js
+++ src/routes/router.js
@@ -7,7 +7,9 @@
 
 import express from 'express'
 import { router as issuesRouter } from './issues-router.js'
+import { router as hookRouter } from './hook-router.js'
 
 export const router = express.Router()
 
 router.use('/', issuesRouter)
+router.use('/webhook', hookRouter)
